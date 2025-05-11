f27f64870a030d34a65e6d7829a3238c41dbd107
Le Duc Nam <arthit@Les-MacBook-Pro.local>
2025-05-11 16:07:24 +0700

    [Base] Base project and UI library

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# freshlife
+# Fresh Life
+
+This project is used for tracking tasks and budgets to save time and make your life happier
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from "react-redux";
+import './index.css'
+import App from './App'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { store } from './redux/store';
+import { CssBaseline } from '@mui/material';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <CssBaseline />
+      <App />
+    </Provider>
+  </StrictMode>,
+)
